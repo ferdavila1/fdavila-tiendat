c7e549a17d3ccb4355f20fb4c9efce724742a019
Ferna707 <fernandojosedavilaherrera0@gmail.com>
2025-09-24 00:14:05 -0600

    semana2
    Hicimos uso de iframes, multimedia

---
+++ src/test/java/com/tiendat/tiendat/TiendatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendat.tiendat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
